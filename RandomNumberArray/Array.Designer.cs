643fce798bccc27ecc3ecd1191cb050708f66444
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-16 14:36:01 -0500

    done.

---
+++ RandomNumberArray/Array.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RandomNumberArray
+{
+    partial class frmRandomNumberArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblSmallestNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(101, 226);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(76, 65);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblSmallestNumber
+            // 
+            this.lblSmallestNumber.AutoSize = true;
+            this.lblSmallestNumber.Location = new System.Drawing.Point(25, 178);
+            this.lblSmallestNumber.Name = "lblSmallestNumber";
+            this.lblSmallestNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblSmallestNumber.TabIndex = 2;
+            // 
+            // frmRandomNumberArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSmallestNumber);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnFind);
+            this.Name = "frmRandomNumberArray";
+            this.Text = "RandomNumberArray";
+            this.Load += new System.EventHandler(this.frmRandomNumberArray_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblSmallestNumber;
+    }
+}
+
---
+++ RandomNumberArray/Array.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 16-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-03
+ * This program displays A lower number in array finder
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberArray
+{
+    public partial class frmRandomNumberArray : Form
+    {
+        public double ArrayFinder (double[] array)
+        {
+            //Variables
+            double smallestNumber = array[0];
+
+            foreach (double numberValue in array)
+            {
+                if(numberValue < smallestNumber)
+                {
+                    smallestNumber = numberValue;
+                }
+                
+            }
+            return smallestNumber;
+        }
+        public frmRandomNumberArray()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRandomNumberArray_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //Variables
+            double smallestNumber;
+            double[] NumberArray = new double[10];
+            Random randomNumberGenerator = new Random();
+
+            //Process
+            for (int counter = 0; counter < 10; counter++)
+            {
+                NumberArray[counter] = randomNumberGenerator.Next(1, 10 + 1);
+                this.lstNumbers.Items.Add(NumberArray[counter]);
+            }
+            smallestNumber = ArrayFinder(NumberArray);
+            this.lblSmallestNumber.Text = "The smallest number is: " + smallestNumber;
+        }
+    }
+}
